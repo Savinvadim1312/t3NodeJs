24f2c8e153cc7f6bd37ed2439eef9e657d39ecc4
Savin Vadim <savinvadim1312@gmail.com>
2019-03-12 11:35:56 +0100

    http

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const ttn = require("ttn");
+var http = require('http');
 
 const appID = "t3saxion";
 const accessKey = "ttn-account-v2.KsIDoQJMmLXj-VpY1Wn03VRinxKfTMbY3RdzeKYbo6w";
+const port = process.env.PORT || 8080;
 
 ttn.data(appID, accessKey)
   .then(function (client) {
@@ -15,3 +17,11 @@ ttn.data(appID, accessKey)
     process.exit(1)
   })
 
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(port); //the server object listens on port 8080
+
+console.log("server listens to port " + port);
